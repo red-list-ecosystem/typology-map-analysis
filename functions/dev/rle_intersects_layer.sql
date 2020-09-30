58940e4b1ac5a05cede5909df4e05e9ccd1be22a
Timo Franz <timo@dumpark.com>
2020-09-30 14:38:31 +0200

    move outdated and alternative functions to subfolder (2)

--- functions/dev/rle_intersects_layer.sql
+++ functions/dev/rle_intersects_layer.sql
@@ -7,22 +7,23 @@ returns boolean
 language plpgsql
 as $function$
 declare
-	rt RECORD;
+	res boolean;
 	tile raster;
 begin
+	res := FALSE;
 	for tile
 	in
 		SELECT rast
-      FROM raster_tiles_bytype as r
-      WHERE lid = r.layer_id
-			AND (occurr IS null OR occurr = r.occurrence)
+        FROM raster_tiles_bytype as r
+        WHERE lid = r.layer_id
+		AND (occurr IS null OR occurr = r.occurrence)
 	loop
-		if
-			ST_Intersects(tile, 1, poly)
-	  then
-			return TRUE;
-	  end if;
+	   if ST_Intersects(tile, 1, poly)
+	   then
+	   		res := TRUE;
+			  exit;
+	   end if;
 	end loop;
-	return false;
+	return res;
 end;
 $function$;
