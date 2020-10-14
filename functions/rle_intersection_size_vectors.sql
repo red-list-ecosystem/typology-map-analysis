b96113b58404d337763475ca739f0d7bab90c2dc
Timo Franz <timo@dumpark.com>
2020-10-14 13:46:41 +0200

    more formatting

--- functions/rle_intersection_size_vectors.sql
+++ functions/rle_intersection_size_vectors.sql
@@ -15,9 +15,9 @@ BEGIN
     SELECT *
     FROM (
       SELECT
-    		l.id AS layer_id,
-    	  vf.occurrence AS occurrence,
-    		SUM (
+        l.id AS layer_id,
+        vf.occurrence AS occurrence,
+        SUM (
           st_area(
             geography(
               st_transform(
@@ -26,17 +26,17 @@ BEGIN
               )
             )
           ) * 0.000001
-    		) AS area
+        ) AS area
     	FROM
-    		vector_features AS vf,
-    		layers AS l
+        vector_features AS vf,
+        layers AS l
     	WHERE vf.layer_id = l.id
         AND l.layer_type = 'vector'
         AND (realm IS null OR realm = l.realm_id)
         AND (biome IS null OR biome = l.biome_id)
         AND (layer IS null OR layer = l.id)
         AND (occurr IS null OR occurr = vf.occurrence)
-    	GROUP BY l.id, vf.occurrence
+      GROUP BY l.id, vf.occurrence
     ) AS layer_areas
     WHERE layer_areas.area > 0
     ORDER BY layer_areas.area DESC;
