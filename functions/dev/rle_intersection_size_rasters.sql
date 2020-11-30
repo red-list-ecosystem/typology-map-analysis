66624b8b9b5f40b5aff4dd8feac247026cd4394e
tmrnz <timo@dumpark.com>
2020-11-30 18:40:49 +0100

    alt versions

---
+++ functions/dev/rle_intersection_size_rasters.sql
@@ -0,0 +1,44 @@
+DROP FUNCTION IF EXISTS rle_intersection_size_rasters;
+
+CREATE OR REPLACE FUNCTION rle_intersection_size_rasters(
+  poly Geometry,
+  realm varchar,
+  biome varchar,
+  layer varchar,
+  occurr int
+)
+RETURNS TABLE (layer_id varchar, occurrence int, area float)
+LANGUAGE plpgsql
+AS $function$
+BEGIN
+  RETURN QUERY
+    SELECT
+      intersections.layer_id,
+      CAST (intersections.occurrence AS Integer),
+      SUM (
+        st_area(
+          geography(
+            st_transform(
+              (intersections.gval).geom,
+              4326
+            )
+          )
+        ) * 0.000001
+      ) AS area
+    FROM (
+        SELECT
+          rt.layer_id,
+          rt.occurrence,
+          st_intersection(rt.rast, 1, poly) AS gval
+        FROM (
+            SELECT intersects.layer_id
+            FROM rle_intersects_rasters(poly, realm, biome, layer, occurr) as intersects
+          ) AS layer_ids,
+          raster_tiles_bytype_test AS rt
+        WHERE layer_ids.layer_id = rt.layer_id
+      ) AS intersections
+    GROUP BY intersections.layer_id, intersections.occurrence
+    ORDER BY area DESC;
+END;
+$function$;
+COMMENT ON FUNCTION rle_intersection_size_rasters IS 'Query vector layers that intersect with given polygon geometry';
---
+++ functions/dev/rle_intersection_size_vectors2.sql
@@ -0,0 +1,42 @@
+DROP FUNCTION IF EXISTS rle_intersection_size_vectors2;
+
+CREATE OR REPLACE FUNCTION rle_intersection_size_vectors2(
+  poly Geometry,
+  realm varchar,
+  biome varchar,
+  layer varchar,
+  occurr int
+)
+RETURNS TABLE (layer_id varchar, occurrence int, area float)
+LANGUAGE plpgsql
+AS $function$
+BEGIN
+  RETURN QUERY
+    SELECT
+      l.id AS layer_id,
+      vf.occurrence AS occurrence,
+      SUM (
+        st_area(
+          geography(
+            st_transform(
+              st_intersection(vf.wkb_geometry, poly),
+              4326
+            )
+          )
+        ) * 0.000001
+      ) AS area
+  	FROM
+      vector_features AS vf,
+	  layers AS l
+	WHERE vf.layer_id = l.id
+	AND l.layer_type = 'vector'
+	AND (realm IS null OR realm = l.realm_id)
+	AND (biome IS null OR biome = l.biome_id)
+	AND (layer IS null OR layer = l.id)
+	AND (occurr IS null OR occurr = vf.occurrence)
+  	AND st_intersects(vf.wkb_geometry, poly)
+    GROUP BY l.id, vf.occurrence
+    ORDER BY area DESC;
+END;
+$function$;
+COMMENT ON FUNCTION rle_intersection_size_vectors2 IS 'Query vector layers that intersect with given polygon geometry';
